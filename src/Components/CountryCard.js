f629f4252693b25eeb3941fb6074fd527c826dda
premnewbie <prempm50@gmail.com>
2024-06-13 15:36:16 +0530

    changed classname

--- src/Components/CountryCard.js
+++ src/Components/CountryCard.js
@@ -2,7 +2,7 @@ import './CountryCard.css';
 
 const CountryCard = ({cardData}) => {
     return ( 
-    <div className="card">
+    <div className="countryCard">
         <img src={cardData.flags.png} alt={cardData.flags.alt} />
         <p>{cardData.name.common}</p>
     </div> );
