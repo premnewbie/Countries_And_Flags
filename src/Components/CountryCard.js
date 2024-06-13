c7a9ff41ac2fdb497e08a60801e0a06426d34cd8
premnewbie <prempm50@gmail.com>
2024-06-13 15:33:36 +0530

    Country names changed to p tag

--- src/Components/CountryCard.js
+++ src/Components/CountryCard.js
@@ -4,7 +4,7 @@ const CountryCard = ({cardData}) => {
     return ( 
     <div className="card">
         <img src={cardData.flags.png} alt={cardData.flags.alt} />
-        <h4>{cardData.name.common}</h4>
+        <p>{cardData.name.common}</p>
     </div> );
 }
  
